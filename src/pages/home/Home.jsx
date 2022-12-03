b08bb3a0055640e45c36ff2cd558e2e021b99155
azimjonking <azimjonjalilov1301@mail.ru>
2022-12-03 18:50:34 +0200

    navbar is done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
 import "./home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
