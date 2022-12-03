c8ee9a46871f52222ed07124bf0f5fe092fae16c
azimjonking <azimjonjalilov1301@mail.ru>
2022-12-03 20:47:37 +0200

    header is done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import "./home.css";
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
